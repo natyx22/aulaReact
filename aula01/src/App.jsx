c6156444d193d6ada141a08e1c9db8d50eb57baa
natyx22 <cristinanataly971@gmail.com>
2026-04-22 14:57:36 -0300

    22/04

---
+++ aula01/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+export default function App()
+{
+  let nome, idade, dias
+  nome= "nataly";
+  idade= 16;
+  dias= idade * 365;
+  return (
+    <div>
+      <h1>Olá mundo !!!</h1>
+      <div className="conteudo">
+        <p>Olá, seja bem vindo ao react com JS.</p>
+        </div>
+    </div>
+  )
+}
