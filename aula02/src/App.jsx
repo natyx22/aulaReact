602fcc41bafcbfe697c837c714365cbf38f8a6c1
natyx22 <cristinanataly971@gmail.com>
2026-05-20 14:50:12 -0300

    20/05

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
 import Exemplo1 from "./paginas/Exemplo1";
+import Exemplo2 from "./paginas/Exemplo2";
+import Exercicio1 from "./paginas/Exercicio1";
+import Exercicio2 from "./paginas/Exercicio2";
+import Exercicio3 from "./paginas/Exercicio3";
+
 
 export default function App()
 {
@@ -14,6 +19,29 @@ export default function App()
         <Exemplo1 numero={2} />
 
       </div>
+      <div className="card">
+        <h3>Chamada para o Exemplo 2</h3>
+        <Exemplo2 numero1={20} numero2={20} />
+        <Exemplo2 numero1={20} numero2={200}/>
+        <Exemplo2 numero1={2.6} numero2={2.7} />
+
+      </div>
+      <div className="card">
+        <h3>Chamada para o Exercicio 1</h3>
+        <Exercicio1 fanhe ={30}/>
+
+      </div>
+      <div className="card">
+      <h3>Chamada para o exercicio 2</h3>
+      <Exercicio2 altura={1.65} peso= {59}/>
+      </div>
+
+      <div className="card">
+      <h3>Chamada para o exercicio 3</h3>
+      <Exercicio3 nota1={5} nota2= {6}/>
+      </div>
+
+     
 
     </div>
   )
--- aula02/src/paginas/Exercicio1.jsx
+++ aula02/src/paginas/Exercicio1.jsx
@@ -1,7 +1,10 @@
-export default function Exercicio1()
+export default function Exercicio1({fanhe})
 {
+    let celsius;
+    celcius=(Number(fanhe)-32)*5/9;
     return (
         <div>
+            A temperatura {fanhe} é {celcius} em celsius
 
         </div>
     )
