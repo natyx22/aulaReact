3260b38c36a24d143a77a67c0a028a15b05ee25b
natyx22 <cristinanataly971@gmail.com>
2026-06-10 11:52:00 -0300

    Update Exercicio4.jsx

--- aula02/src/paginas/Exercicio4.jsx
+++ aula02/src/paginas/Exercicio4.jsx
@@ -6,3 +6,41 @@ export default function Exercicio4()
         </div>
     )
 }
+
+
+
+export default function Exercicio8( {dis, com} )
+{ 
+
+  let consu= Number(dis) / Number(com);
+
+ 
+   
+
+  if (consumo >= 12)
+  {
+    return (
+        <div>
+          ECONOMICO
+        </div>
+      )
+  }
+  else if (consumo >= 8)
+  {
+    return (
+        <div>
+          MODERADO
+        </div>
+      )
+  }
+  else
+  {
+    return (
+        <div>
+          GASTADOR
+        </div>
+      )
+  }
+
+  )
+}
